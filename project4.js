7ac6f33355f58586fc7833aff031deeb4bc20114
Jaspreet2022 <107615553+Jaspreet2022@users.noreply.github.com>
2022-09-11 14:25:53 +0530

    Add files via upload

---
+++ project4.js
@@ -0,0 +1,16 @@
+let btn1 =document.getElementById("btn1");
+btn1.addEventListener('click',function(){
+    document.body.style.background="aqua";
+});
+let btn2 =document.getElementById("btn2");
+btn2.addEventListener('click',function(){
+    document.body.style.background="brown";
+});
+let btn3 =document.getElementById("btn3");
+btn3.addEventListener('click',function(){
+    document.body.style.background="black";
+});
+let btn4 =document.getElementById("btn4");
+btn4.addEventListener('click',function(){
+    document.body.style.background="pink";
+});
